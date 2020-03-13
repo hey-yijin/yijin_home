93c33c53dba2d94ec68d9df893dcd2018dea8cc7
hey-yijin <61109098+hey-yijin@users.noreply.github.com>
2020-03-13 11:38:50 +0800

    Update and rename README.md to vue.js

---
+++ vue.js
@@ -0,0 +1,17 @@
+#Vue笔记
+##vue是什么
+1.vue前端框架，React前端框架，可以开发web，手机app
+2.三大主流框架 vue.js Angular.js,React.js
+3.vue。js只关注视图层
+4.前端主要工作，只关注MVC中的V这一层
+## 为什么要学习框架
++开发效率提高
+
++原生js->Jquery类库->前端模板引擎->Agular.js/Vue.js（减少不必要的dom操作，调高绚烂效率）
+##框架和库的区别
+ + 框架：是一套完整的解决方案，对项目侵入大；
+ + 库：提供一个小功能，对项目侵入小
+##node中的MVC和MVVM前端的区别
+ + MVC
+ + MVVM model view vm（viewModel）
+ + MVVM model（每个页面中的数据） --mv--view （界面） （双向绑定）
